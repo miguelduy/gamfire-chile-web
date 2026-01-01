7e4abbf88826c9faf645b7911003cb2bfc3738f1
Miguel Ignacio Duyvestein López <miguel.duyvestein@interior.gob.cl>
2026-01-01 20:34:45 -0300

    Fix: Add base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/gamfire-chile-web/',
   build: {
     rollupOptions: {
       input: {
